3ca0a1361d01460c67b17c415ae48e44de541658
Umut Seven <umutseven92@gmail.com>
2013-10-06 23:45:37 +0300

    Initial commit

---
+++ Romero.Windows/Player.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Romero.Windows
+{
+    class Player : Sprite
+    {
+        const string PlayerAssetName = "player";
+        const int StartPositionX = 125;
+        const int StartPositionY = 245;
+        const int PlayerSpeed = 300;
+        const int MoveUp = -1;
+        const int MoveDown = 1;
+        const int MoveLeft = -1;
+        const int MoveRight = 1;
+        const float SprintModifier = 2.0f;
+
+        enum State
+        {
+            Running,
+            Sprinting
+        }
+
+        State _currentState = State.Running;
+
+        Vector2 _direction = Vector2.Zero;
+        Vector2 _speed = Vector2.Zero;
+
+        private bool gamepadConnected = true;
+        KeyboardState _previousKeyboardState;
+        private GamePadState _previousGamePadState;
+
+        public void LoadContent(ContentManager contentManager)
+        {
+            SpritePosition = new Vector2(StartPositionX, StartPositionY);
+            LoadContent(contentManager, PlayerAssetName);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+            var currentKeyboardState = Keyboard.GetState();
+            var currentGamepadState = GamePad.GetState(PlayerIndex.One);
+
+            UpdateMovement(currentKeyboardState, currentGamepadState);
+
+            _previousKeyboardState = currentKeyboardState;
+            _previousGamePadState = currentGamepadState;
+
+            Update(gameTime, _speed, _direction);
+        }
+
+        private void UpdateMovement(KeyboardState currentKeyboardState, GamePadState currentGamePadState)
+        {
+            switch (gamepadConnected)
+            {
+                #region Gamepad
+                case true:
+                    if (currentGamePadState.IsButtonDown(Buttons.A))
+                    {
+                        _currentState = State.Sprinting;
+                    }
+                    else
+                    {
+                        _currentState = State.Running;
+                    }
+
+                    switch (_currentState)
+                    {
+                        #region Running
+                        case State.Running:
+                            _speed = Vector2.Zero;
+                            _direction = Vector2.Zero;
+                            if (currentGamePadState.ThumbSticks.Left.X <= -0.3)
+                            {
+                                _speed.X = PlayerSpeed;
+                                _direction.X = MoveLeft;
+                            }
+
+                            else if (currentGamePadState.ThumbSticks.Left.X >= 0.3)
+                            {
+                                _speed.X = PlayerSpeed;
+                                _direction.X = MoveRight;
+                            }
+
+                            if (currentGamePadState.ThumbSticks.Left.Y >= 0.3)
+                            {
+                                _speed.Y = PlayerSpeed;
+                                _direction.Y = MoveUp;
+                            }
+
+                            else if (currentGamePadState.ThumbSticks.Left.Y <= -0.3)
+                            {
+                                _speed.Y = PlayerSpeed;
+                                _direction.Y = MoveDown;
+                            }
+                            break;
+                        #endregion
+                        #region Sprinting
+                        case State.Sprinting:
+                            _speed = Vector2.Zero;
+                            _direction = Vector2.Zero;
+                            if (currentGamePadState.ThumbSticks.Left.X <= -0.3)
+                            {
+                                _speed.X = PlayerSpeed * SprintModifier;
+                                _direction.X = MoveLeft;
+                            }
+
+                            else if (currentGamePadState.ThumbSticks.Left.X >= 0.3)
+                            {
+                                _speed.X = PlayerSpeed * SprintModifier;
+                                _direction.X = MoveRight;
+                            }
+
+                            if (currentGamePadState.ThumbSticks.Left.Y >= 0.3)
+                            {
+                                _speed.Y = PlayerSpeed * SprintModifier;
+                                _direction.Y = MoveUp;
+                            }
+
+                            else if (currentGamePadState.ThumbSticks.Left.Y <= -0.3)
+                            {
+                                _speed.Y = PlayerSpeed * SprintModifier;
+                                _direction.Y = MoveDown;
+                            }
+                            break;
+                        #endregion
+                    }
+
+                    break; 
+                #endregion
+                #region Keyboard
+                case false:
+                    if (currentKeyboardState.IsKeyDown(Keys.LeftShift))
+                    {
+                        _currentState = State.Sprinting;
+                    }
+                    else
+                    {
+                        _currentState = State.Running;
+                    }
+
+                    switch (_currentState)
+                    {
+                        #region Running
+                        case State.Running:
+                            _speed = Vector2.Zero;
+                            _direction = Vector2.Zero;
+                            if (currentKeyboardState.IsKeyDown(Keys.A))
+                            {
+                                _speed.X = PlayerSpeed;
+                                _direction.X = MoveLeft;
+                            }
+                            else if (currentKeyboardState.IsKeyDown(Keys.D))
+                            {
+                                _speed.X = PlayerSpeed;
+                                _direction.X = MoveRight;
+                            }
+                            if (currentKeyboardState.IsKeyDown(Keys.W))
+                            {
+                                _speed.Y = PlayerSpeed;
+                                _direction.Y = MoveUp;
+                            }
+                            else if (currentKeyboardState.IsKeyDown(Keys.S))
+                            {
+                                _speed.Y = PlayerSpeed;
+                                _direction.Y = MoveDown;
+                            }
+                            break;
+                        #endregion
+                        #region Sprinting
+                        case State.Sprinting:
+                            _speed = Vector2.Zero;
+                            _direction = Vector2.Zero;
+                            if (currentKeyboardState.IsKeyDown(Keys.A))
+                            {
+                                _speed.X = PlayerSpeed * SprintModifier;
+                                _direction.X = MoveLeft;
+                            }
+                            else if (currentKeyboardState.IsKeyDown(Keys.D))
+                            {
+                                _speed.X = PlayerSpeed * SprintModifier;
+                                _direction.X = MoveRight;
+                            }
+                            if (currentKeyboardState.IsKeyDown(Keys.W))
+                            {
+                                _speed.Y = PlayerSpeed * SprintModifier;
+                                _direction.Y = MoveUp;
+                            }
+                            else if (currentKeyboardState.IsKeyDown(Keys.S))
+                            {
+                                _speed.Y = PlayerSpeed * SprintModifier;
+                                _direction.Y = MoveDown;
+                            }
+                            break;
+                        #endregion
+                    }
+                    break;
+                
+                #endregion
+            }
+
+
+        }
+    }
+}
---
+++ Romero.Windows/Sprite.cs
@@ -0,0 +1,60 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Romero.Windows
+{
+    class Sprite
+    {
+        //The current position of the Sprite
+        public Vector2 SpritePosition = new Vector2(0, 0);
+
+        //The texture object used when drawing the sprite
+        private Texture2D _spriteTexture2D;
+
+        //The asset name for the Sprite's Texture
+        public string AssetName;
+
+        //The Size of the Sprite (with scale applied)
+        public Rectangle Size;
+
+        //The amount to increase/decrease the size of the original sprite. 
+        private float _scale = 1.0f;
+
+        //When the scale is modified throught he property, the Size of the 
+        //sprite is recalculated with the new scale applied.
+        public float ScaleCalc
+        {
+            get { return _scale; }
+            set
+            {
+                _scale = value;
+                //Recalculate the Size of the Sprite with the new scale
+                Size = new Rectangle(0, 0, (int) (_spriteTexture2D.Width*_scale), (int) (_spriteTexture2D.Height*_scale));
+            }
+        }
+
+        //Load the texture for the sprite using the Content Pipeline
+        public void LoadContent(ContentManager contentManager, string assetName)
+        {
+            _spriteTexture2D = contentManager.Load<Texture2D>(assetName);
+            AssetName = assetName;
+            Size = new Rectangle(0, 0, (int)(_spriteTexture2D.Width * ScaleCalc), (int)(_spriteTexture2D.Height * ScaleCalc));
+        }
+
+        //Draw the sprite to the screen
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_spriteTexture2D, SpritePosition,
+                new Rectangle(0, 0, _spriteTexture2D.Width, _spriteTexture2D.Height),
+                Color.White, 0.0f, Vector2.Zero, ScaleCalc, SpriteEffects.None, 0);
+        }
+
+        //Update the Sprite and change it's position based on the passed in speed, direction and elapsed time.
+        public void Update(GameTime gameTime, Vector2 speed, Vector2 direction)
+        {
+            SpritePosition += direction * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+}
